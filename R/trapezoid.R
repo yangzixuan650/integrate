a31f415813d0926f945cdfe9f3c841a56b647493
yangzixuan650 <yangsharon650@gmail.com>
2022-11-08 17:58:17 -0500

    Add documentation to trapezoid.R

--- R/trapezoid.R
+++ R/trapezoid.R
@@ -1,8 +1,18 @@
+#' Title
+#'
+#' @param f
+#' @param range
+#' @param n
+#'
+#' @return
+#' @export
+#'
+#' @examples
 trapezoid <- function(f, range, n){
   res <- 0
+  a <- range[1]
+  b <- range[2]
   for(i in 1:n){
-    a <- range[1]
-    b <- range[2]
     res <- res + (f(a+(i/n) + f(a+(i+1)/n)))*(b - a)/(2*n)
   }
   return(res)
